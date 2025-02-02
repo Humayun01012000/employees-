b69598da34d25b6897b89c3244a91747ff0bbee7
code lover <codelovemehk@gmail.com>
2025-02-02 23:30:09 +0600

    office

--- home/models.py
+++ home/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 
-# Create your models here. 
 
 
+
+# Employee Model
 class Employee(models.Model):
-    employee_id = models.PositiveBigIntegerField()
+    employee_id = models.PositiveBigIntegerField(unique=True)  # Ensure unique Employee ID
     name = models.CharField(max_length=255)
-    department = models.CharField(max_length=255)
-    position = models.CharField(max_length=255)
+    department = models.CharField(max_length=100)
+    position =  models.CharField(max_length=100)
     floor = models.CharField(max_length=100)
     salary = models.FloatField()
- 
+
     def __str__(self):
-        return f"Name: {self.name} Department: {self.department}"
+        return f"{self.name} - {self.department.name} - {self.position.name}"
+
+
